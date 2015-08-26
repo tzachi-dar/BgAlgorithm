c052ea9663f957790a377038424b4c9d2fb3fa04
JacoCronje <cronje.jc@gmail.com>
2015-08-26 11:49:39 +0200

    Plot calculated bg against raw and calibration values

--- plot_sensor.sh
+++ plot_sensor.sh
@@ -9,7 +9,8 @@ echo set xlabel \"Time in days\" >>plotfile.p
 echo set ylabel \"BG\" >>plotfile.p
 echo set key right top >>plotfile.p
 echo set grid >>plotfile.p
-echo plot \"sensor$1_raw.csv\" using 1:2 title \'raw values\' with linespoints, \\ >>plotfile.p
-echo      \"sensor$1_calib.csv\" using 1:2 title \'calibration\' with points >>plotfile.p
+echo plot \"sensor$1_raw.csv\" using 1:2 title \'raw values\' with lines, \\ >>plotfile.p
+echo      \"sensor$1_calc.csv\" using 1:2 title \'calculated bg\' with lines, \\ >>plotfile.p
+echo      \"sensor$1_calib.csv\" using 1:2 title \'calibration\' with points pt 7 >>plotfile.p
 echo pause -1 >>plotfile.p
 gnuplot plotfile.p
